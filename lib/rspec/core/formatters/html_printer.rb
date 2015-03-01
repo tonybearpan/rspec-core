dc9941a63192baa0877d13314e1fe2a5791bfa1e
SunLn <dengchenhua@gmail.com>
2015-03-01 22:28:01 +0800

    fix confict

--- lib/rspec/core/formatters/html_printer.rb
+++ lib/rspec/core/formatters/html_printer.rb
@@ -115,11 +115,8 @@ module RSpec
   </div>
 
   <div id="display-filters">
-<<<<<<< HEAD
-=======
     <input id="toggle_checkbox"  name="toggle_checkbox"  type="checkbox" onchange="toggle_menu()" value="1" /> <label for="toggle_checkbox">toogle menu</label>
 
->>>>>>> update, add toggle all menu feature
     <input id="passed_checkbox"  name="passed_checkbox"  type="checkbox" checked="checked" onchange="apply_filters()" value="1" /> <label for="passed_checkbox">Passed</label>
     <input id="failed_checkbox"  name="failed_checkbox"  type="checkbox" checked="checked" onchange="apply_filters()" value="2" /> <label for="failed_checkbox">Failed</label>
     <input id="pending_checkbox" name="pending_checkbox" type="checkbox" checked="checked" onchange="apply_filters()" value="3" /> <label for="pending_checkbox">Pending</label>
